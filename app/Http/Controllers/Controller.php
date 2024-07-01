27c137a5d664584bb39e922c00b037032ec0e503
Iago Oliveira <ioads@outlook.com>
2024-07-01 19:39:23 -0300

    cria documentação do controller flight

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,21 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="API de Passagens áereas",
+ *      description="API de compras de passagens aéreas",
+ *      @OA\Contact(
+ *          email="ioads@outlook.com",
+ *          name="Iago Oliveira"
+ *      ),
+ *      @OA\License(
+ *          name="MIT License",
+ *          url="https://opensource.org/licenses/MIT"
+ *      )
+ * )
+ */
 abstract class Controller
 {
     //
--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -19,7 +19,15 @@ class FlightController extends Controller
 
 
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *      path="/api/flights",
+     *      operationId="getFlightsList",
+     *      tags={"Flights"},
+     *      summary="Get list of flights",
+     *      description="Returns list of flights",
+     *      @OA\Response(response=200, description="Successful operation"),
+     *      @OA\Response(response=400, description="Bad request")
+     * )
      */
     public function index()
     {
@@ -29,7 +37,32 @@ class FlightController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *      path="/api/flights",
+     *      operationId="storeFlight",
+     *      tags={"Flights"},
+     *      summary="Store flight",
+     *      description="Store a flight",
+    *      @OA\RequestBody(
+    *          required=true,
+    *          description="Create Flight",
+    *          @OA\JsonContent(
+    *              @OA\Property(property="origin_id", type="string", example="1"),
+    *              @OA\Property(property="destination_id", type="string", format="email", example="2"),
+    *              @OA\Property(property="code", type="string", example="1234"),
+    *              @OA\Property(property="departure", type="date", example="2024-08-25 12:00:00"),
+    *              @OA\Property(property="classes", type="array", 
+    *                  @OA\Items(
+    *                      @OA\Property(property="class_id", type="integer", example=1),
+    *                      @OA\Property(property="seats", type="integer", example="30"),
+    *                      @OA\Property(property="unit_price", type="float", example="1500.50"),
+    *                  ),
+    *              ),
+    *          )
+    *      ),
+     *      @OA\Response(response=200, description="Successful operation"),
+     *      @OA\Response(response=400, description="Bad request")
+     * )
      */
     public function store(StoreFlightRequest $request)
     {
@@ -41,7 +74,25 @@ class FlightController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *      path="/api/flights/{flight}",
+     *      operationId="updateFlight",
+     *      tags={"Flights"},
+     *      summary="Update flight",
+     *      description="Update a flight",
+    *      @OA\RequestBody(
+    *          required=true,
+    *          description="Update a Flight",
+    *          @OA\JsonContent(
+    *              @OA\Property(property="origin_id", type="string", example="1"),
+    *              @OA\Property(property="destination_id", type="string", format="email", example="2"),
+    *              @OA\Property(property="code", type="string", example="1234"),
+    *              @OA\Property(property="departure", type="date", example="2024-08-25 12:00:00"),
+    *          )
+    *      ),
+     *      @OA\Response(response=200, description="Successful operation"),
+     *      @OA\Response(response=400, description="Bad request")
+     * )
      */
     public function update(Flight $flight, UpdateFlightRequest $request)
     {
@@ -53,13 +104,40 @@ class FlightController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *      path="/api/flights/{flight}",
+     *      operationId="cancelFlight",
+     *      tags={"Flights"},
+     *      summary="Cancel flight",
+     *      description="Cancel a flight",
+    *      @OA\RequestBody(
+    *          required=true,
+    *          description="Cancel a Flight",
+    *      ),
+     *      @OA\Response(response=200, description="Successful operation"),
+     *      @OA\Response(response=400, description="Bad request")
+     * )
      */
     public function destroy(Flight $flight)
     {
         return $this->flightRepository->delete($flight);
     }
 
+    /**
+     * @OA\Get(
+     *      path="/api/flights/passengers/{flight}",
+     *      operationId="passengersFlight",
+     *      tags={"Flights"},
+     *      summary="Get passengers by flight",
+     *      description="Get passengers by flight",
+    *      @OA\RequestBody(
+    *          required=true,
+    *          description="Get passengers by Flight",
+    *      ),
+     *      @OA\Response(response=200, description="Successful operation"),
+     *      @OA\Response(response=400, description="Bad request")
+     * )
+     */
     public function passengers(string $flightId)
     {
         $passengers = $this->flightRepository->passengers($flightId);
